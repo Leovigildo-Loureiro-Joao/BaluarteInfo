3312980e2ed831fedda791be30bf3f7e1cdaf260
Leovigildo Programmer <leovigildojoao902@gmail.com>
2025-04-04 03:17:18 +0100

    encerrando as mensagens

--- api/src/main/java/com/igreja/api/repositories/MensagemRepository.java
+++ api/src/main/java/com/igreja/api/repositories/MensagemRepository.java
@@ -3,7 +3,10 @@ package com.igreja.api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.igreja.api.models.MensagensModel;
+import java.util.List;
+import com.igreja.api.enums.MensagemType;
+
 
 public interface MensagemRepository extends JpaRepository<MensagensModel,Integer>{
-    
+    List<MensagensModel> findByTipo(MensagemType tipo);
 }
