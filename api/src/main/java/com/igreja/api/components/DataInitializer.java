c70ba24e640e2f61c6efc21a405337ddf4694b7d
Leovigildo Programmer <leovigildojoao902@gmail.com>
2025-04-05 02:33:05 +0100

    encerrando config

--- api/src/main/java/com/igreja/api/components/DataInitializer.java
+++ api/src/main/java/com/igreja/api/components/DataInitializer.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Component;
 
 import com.igreja.api.models.UserModel;
 import com.igreja.api.repositories.UserRepository;
+import com.igreja.api.services.ConfigService;
 
 @Component
 public class DataInitializer implements CommandLineRunner{
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ConfigService configService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -22,8 +26,9 @@ public class DataInitializer implements CommandLineRunner{
         if (userRepository.count() == 0) {
             UserModel admin = new UserModel("admin", passwordEncoder.encode("1234"),"leovigildojoao902@gmail.com", "ADMIN,USER");
             userRepository.save(admin); 
-            
+                        
         }
     }
+
 
 }
---
+++ api/src/main/java/com/igreja/api/controllers/ConfigController.java
@@ -0,0 +1,31 @@
+package com.igreja.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.igreja.api.dto.comentario.ComentarioDto;
+import com.igreja.api.dto.config.ValueConfigDto;
+import com.igreja.api.services.ConfigService;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class ConfigController {
+
+    @Autowired
+    private ConfigService cpConfigService;
+
+     @PutMapping("/user/config/edit")
+    public ResponseEntity<?> EditComentario(@RequestBody @Valid ValueConfigDto value) {    
+       try {
+            return ResponseEntity.ok(cpConfigService.edit(value));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
