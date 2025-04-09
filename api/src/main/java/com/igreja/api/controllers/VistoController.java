6f811b699fc519e30913235a525974423ec92123
Leovigildo Programmer <leovigildojoao902@gmail.com>
2025-04-09 15:27:27 +0100

    testando os os puts e posts

--- api/src/main/java/com/igreja/api/controllers/VistoController.java
+++ api/src/main/java/com/igreja/api/controllers/VistoController.java
@@ -3,13 +3,10 @@ package com.igreja.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.igreja.api.models.AcessoModel;
-import com.igreja.api.models.ArtigosModel;
-import com.igreja.api.repositories.AcessoRepository;
 import com.igreja.api.repositories.ArtigosRepository;
 import com.igreja.api.repositories.MidiaRepository;
 import com.igreja.api.repositories.VistosRepository;
@@ -28,14 +25,14 @@ public class VistoController {
     private MidiaRepository midiaRepository;
 
 
-    @GetMapping
-    public ResponseEntity<Long> contarVisualizacaoesArtigo( int idArtigo ) {
+    @GetMapping("/user/vistos/artigo/{id}")
+    public ResponseEntity<Long> contarVisualizacaoesArtigo(@PathVariable(name = "id") int idArtigo ) {
         long total = vistosRepository.findByArtigo(artigosRepository.findById(idArtigo).get()).size();
         return ResponseEntity.ok(total);
     }
 
-    @GetMapping
-    public ResponseEntity<Long> contarVisualizacaoesMidia( int midia ) {
+    @GetMapping("/user/vistos/midia/{id}")
+    public ResponseEntity<Long> contarVisualizacaoesMidia(@PathVariable(name = "id") int midia ) {
         long total = vistosRepository.findByMidia(midiaRepository.findById(midia).get()).size();
         return ResponseEntity.ok(total);
     }
