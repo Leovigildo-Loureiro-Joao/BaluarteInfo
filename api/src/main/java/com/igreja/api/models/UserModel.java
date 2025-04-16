d57d6be3af3dd503a651e69bc868f465c04da2da
Leovigildo João <leovigildojoao902@gmail.com>
2025-04-16 16:08:16 +0100

    Usando o linux

--- api/src/main/java/com/igreja/api/models/UserModel.java
+++ api/src/main/java/com/igreja/api/models/UserModel.java
@@ -66,6 +66,26 @@ public class UserModel implements UserDetails{
     public String getUsername() {
         return this.email;
     }
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+    }
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+    }
 
     
 
