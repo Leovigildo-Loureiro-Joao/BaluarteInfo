618a2955f97af804a8d945f56b1988c115a9396b
Leovigildo João <leovigildojoao902@gmail.com>
2025-07-22 14:24:55 +0100

    Add Duracao de actividade 'MENSAL,ANUAL,PROJECTO'

--- api/src/main/java/com/igreja/api/dto/actividade/ActividadeDto.java
+++ api/src/main/java/com/igreja/api/dto/actividade/ActividadeDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.igreja.api.enums.ActividadeType;
+import com.igreja.api.enums.DuracaoActividade;
 import com.igreja.api.enums.PublicoAlvoType;
 
 import jakarta.persistence.EnumType;
@@ -24,6 +25,7 @@ public record ActividadeDto(
    @Enumerated(EnumType.STRING) ActividadeType tipoEvento,
 
    @Enumerated(EnumType.STRING) PublicoAlvoType publicoAlvo,
+   @Enumerated(EnumType.STRING) DuracaoActividade duracao,
 
    @NotBlank String organizador,
 
