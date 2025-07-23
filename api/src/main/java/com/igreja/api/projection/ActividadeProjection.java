f361d62ff4acccfda2da804b42f4eb44ec862983
Leovigildo João <leovigildojoao902@gmail.com>
2025-07-23 14:33:53 +0100

    add duracao

--- api/src/main/java/com/igreja/api/projection/ActividadeProjection.java
+++ api/src/main/java/com/igreja/api/projection/ActividadeProjection.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.igreja.api.enums.ActividadeType;
+import com.igreja.api.enums.DuracaoActividade;
 import com.igreja.api.enums.PublicoAlvoType;
 
 import jakarta.persistence.EnumType;
@@ -25,6 +26,8 @@ public interface ActividadeProjection{
     String getEndereco();
 
     ActividadeType getTipoEvento();
+
+    DuracaoActividade getDuracao();
 
     PublicoAlvoType getPublicoAlvo();
 
