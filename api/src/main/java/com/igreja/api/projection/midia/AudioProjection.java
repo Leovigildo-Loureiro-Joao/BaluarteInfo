7dd90cdf4874672f1e46edfc62069dc60e16d8a3
Leovigildo João <leovigildojoao902@gmail.com>
2025-08-15 08:59:03 +0100

    Aleterando o retotno do audio

--- api/src/main/java/com/igreja/api/projection/midia/AudioProjection.java
+++ api/src/main/java/com/igreja/api/projection/midia/AudioProjection.java
@@ -1,10 +1,13 @@
 package com.igreja.api.projection.midia;
 
+import com.igreja.api.enums.AudioType;
+
 public interface AudioProjection {
     Integer getId();
     String getTitulo();
     String getDescricao();
     String getImagem();
     String getTempo();
+    AudioType getAudioType();
     String getUrl();
 }
